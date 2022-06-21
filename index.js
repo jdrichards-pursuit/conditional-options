c61dd419bcd94a305dcf7fb80ab0fc21821f70ed
jd richards <jdrichardstech@gmail.com>
2022-06-21 08:30:29 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+/*
+A ternary operator takes three operands: 
+1 - a condition followed by a question mark (?), 2- then an expression to execute if the condition is truthy followed by a colon (:), 
+3- and finally the expression to execute if the condition is falsy. This operator is frequently used as an alternative to an if...else statement.
+
+
+condition (no if) ? truthy : falsy
+*/
+
+/**
+ * seeMovie()
+ * ------------
+ * Takes an age parameter and returns whether a person is old enough to see the movie.
+ * If the person is old enough, return a string
+ * saying: 'Enjoy the movie!'.
+ * If not, return a string saying:
+ * 'Come back when you are older.'
+ * @param {number} age
+ * @returns {string}
+ */
+
+function seeMovie() {}
+
+/**
+ * loggedIn()
+ * -----------------
+ * Takes in a boolean that represents whether the
+ * user is logged in. If the user is logged in,
+ * returns 'Welcome to my website'. If not,
+ * returns, 'Please sign up or log in'
+ *
+ * @param {boolean} isLogged
+ * @returns {string}
+ */
+
+function isLogged() {}
+
+//switch/case
+// evaluates an expression, matching the expression's value to a case clause, and executes statements associated with that case
+
+/**
+ *
+ * costOfFruit()
+ * ---------------
+ * Takes in the name of a fruit. If the fruit is in stock, it will return the cost of that fruit. If the fruit is 'apple', it will return a string says, 'Apples are $2.79 a pound'. If the fruit is orange it will return a string that says:
+ * 'Oranges are $3.00 a pound'. If it says bananas it will return 'Bananas are 75 cents a pound. Otherwise, it will return, 'Sorry, we are all out of <name of fruit>'
+ * @param {string} fruit
+ * @returns {string}
+ */
+
+costOfFruit();
+
+/**
+ * wonRaffle()
+ * ------------
+ * Takes in a number. If the number matches one
+ * of the numbers, returns a message based on
+ * that number. If the number is 1 returns, "You
+ * win first prize". If the number is 2, returns:
+ * 'You win second prize'. If the number is 3,
+ * returns: 'You win third prize', Otherwise, it
+ * will return: 'Sorry, you are not a winnder'
+ * @param {number} ticket
+ * @returns {string}
+ */
+
+wonRaffle();
